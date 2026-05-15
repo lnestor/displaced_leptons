d2b28da554728db682a808085ec4c2e400e260b2
Lucas Nestor <nestor.40@osu.edu>
2026-05-15 09:05:47 -0400

    Rework generic plotting script to include 2D plots

--- scripts/plotting/plot_generic.py
+++ scripts/plotting/plot_generic.py
@@ -1,37 +1,30 @@
 import argparse
 import coffea.util
-import hist
 import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
 import mplhep as hep
+import sys
+from util import (
+    add_common_args,
+    apply_common_args_to_ax,
+    apply_common_args_to_hist,
+    get_variables,
+    get_samples,
+    get_years,
+    get_datasets,
+    get_categories,
+    get_combined_hist,
+    get_is_data
+)
 
 hep.style.use("CMS")
 
-def _get_hist(root, variable, sample, year_key, category, is_data):
-    if is_data:
-        return root["variables"][variable][sample][year_key][category, :]
-    else:
-        return root["variables"][variable][sample][year_key][category, "nominal", :]
 
-def _combine_hists(root, variable, sample, year, category, is_data):
-    combined = None
-    for year_key in root["variables"][variable][sample]:
-        if year in year_key:
-            h = _get_hist(root, variable, sample, year_key, category, is_data)
-
-            if combined is None:
-                combined = h
-            else:
-                combined += h
-
-    return combined
-
-def plot(root, output, variable, sample, year, category, is_data, **kwargs):
-    h = _combine_hists(root, variable, sample, year, category, is_data)
-
+def plot(h, output, is_data, **kwargs):
     fig, ax = plt.subplots()
     hep.cms.label("Preliminary", ax=ax, data=is_data, loc=1) # loc=1 is top left interior
     hep.histplot(
-        h[hist.loc(0 if kwargs["xstart"] is None else kwargs["xstart"]):],
+        h,
         histtype="errorbar",
         ax=ax,
         markersize=8,
@@ -40,44 +33,49 @@ def plot(root, output, variable, sample, year, category, is_data, **kwargs):
         flow="none" # Prevents placing a "step connection" to the first visible point
     )
 
-    if kwargs["xlim"][0] is not None:
-        ax.set_xlim(left=kwargs["xlim"][0])
-
-    if kwargs["xlim"][1] is not None:
-        ax.set_xlim(right=kwargs["xlim"][1])
-
-    if kwargs["ylim"][0] is not None:
-        ax.set_ylim(bottom=kwargs["ylim"][0])
-
-    if kwargs["ylim"][1] is not None:
-        ax.set_ylim(top=kwargs["ylim"][1])
-
-    if kwargs["xlog"]:
-        ax.set_xscale("log")
-
-    if kwargs["ylog"]:
-        ax.set_yscale("log")
+    apply_common_args_to_ax(ax, **kwargs)
 
     unit = h.axes[0].label.split("[")[1][0:-1]
     ax.set_ylabel(f"Entries / {h.axes[0].widths[0]:.1f} {unit}")
 
     fig.savefig(output)
 
+
+def plot2d(h, output, is_data, **kwargs):
+    fig, ax = plt.subplots()
+    hep.cms.label("Preliminary", ax=ax, data=is_data, loc=1) # loc=1 is top left interior
+    hep.hist2dplot(h, ax=ax, norm=LogNorm())
+
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+    ax.set_xlim(1, 200000)
+    ax.set_ylim(1, 200000)
+    ticks = [100, 500, 100000]
+    ax.set_xticks(ticks)
+    ax.set_yticks(ticks)
+    ax.xaxis.set_major_formatter(plt.ScalarFormatter())
+    ax.yaxis.set_major_formatter(plt.ScalarFormatter())
+
+    apply_common_args_to_ax(ax, **kwargs)
+
+    fig.savefig(output)
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="Path to .coffea file")
-    parser.add_argument("-o", "--output", help="Output filename. Defaults to <DEFUALT")
-    parser.add_argument("-v", "--variable", required=True)
+    parser.add_argument("hist", help="Name of the histogram to plot")
+
     parser.add_argument("-c", "--category")
-    parser.add_argument("-y", "--year")
+    parser.add_argument("-y", "--years", nargs="+")
     parser.add_argument("-s", "--sample")
-    parser.add_argument("--xmin", type=float, default=None)
-    parser.add_argument("--xmax", type=float, default=None)
-    parser.add_argument("--ymin", type=float, default=None)
-    parser.add_argument("--ymax", type=float, default=None)
-    parser.add_argument("--xlog", action="store_true")
-    parser.add_argument("--ylog", action="store_true")
-    parser.add_argument("--xstart", type=float, default=None)
+
+    parser.add_argument("-o", "--output", default="plot.png", help="Output filename.")
+
+    parser.add_argument("--xvar", help="Name of the variable to plot on the xaxis for 2D histograms")
+    parser.add_argument("--yvar", help="Name of the varaible to plot on the yaxis for 2D histograms")
+
+    add_common_args(parser)
     # parser.add_argument("-n", "--normalize", action="store_true")
     # parser.add_argument("-ps", "--plot-separately", action="store_true")
     args = parser.parse_args()
@@ -85,50 +83,79 @@ def main():
     print(f"Loading {args.input}")
     f = coffea.util.load(args.input)
 
-    valid_vars = f["variables"].keys()
-    if args.variable not in valid_vars:
-        parser.error(f"Unknown variable: {args.variable}. Choose from {', '.join(valid_vars)}")
-
-    """
-    samples = valid_samples = f["variables"][args.variable].keys()
-    if args.sample is None:
-        print(f"Sample not provided. Using all: {', '.join(valid_samples)}")
+    valid_hists = get_variables(f)
+    if not args.hist in valid_hists:
+        print(f"\nUnknown histogram '{args.hist}' selected."
+              f" Choose from:\n  {'\n  '.join(valid_hists)}\n")
+        sys.exit(1)
+    
+    valid_samples = get_samples(f, args.hist)
+    if args.sample and args.sample not in valid_samples:
+        print(f"\nUnknown sample '{args.sample}' selected."
+              f" Choose from:\n  {'\n  '.join(valid_samples)}")
+        sys.exit(1)
+    elif not args.sample:
+        samples = valid_samples
+        print(f"Sample not provided."
+              f" Will combine all:\n  {'\n  '.join(valid_samples)}")
     else:
-        if args.sample not in valid_samples:
-            parser.error(f"Unknown sample: {args.sample}. Choose from {', '.join(valid_samples)}")
-        else:
-            print(f"Using sample {args.sample}")
-            samples = [args.sample]
+        # TODO: support multiple samples pass in
+        samples = [args.sample]
+        print(f"Using sample: {args.sample}")
 
-    # For now: assume all years are present on all samples
-    years = valid_years = f["variables"][args.variable][samples[0]].keys()
-    if args.year is None:
-        print(f"Year not provided. Using all: {', '.join(valid_years)}")
+    # Assume all samples have same year for now
+    valid_years = get_years(f, args.hist, samples[0])
+    if args.years and any(y not in valid_years for y in args.years):
+        invalid_years = [y for y in args.years if not y in valid_years]
+        print(f"\nUnknown year(s) '{' '.join(invalid_years)}' selected."
+              f"Choose from:\n  {'\n  '.join(sorted(valid_years))}")
+        sys.exit(1)
+    elif not args.years:
+        years = valid_years
+        print(f"Years not provided."
+              f" Will combine all:\n  {'\n  '.join(valid_years)}")
     else:
-        if args.year not in valid_years:
-            parser.error(f"Unknown year: {args.year}. Choose from {', '.join(valid_years)}")
-        else:
-            print(f"Using year {args.year}")
-            years = [args.year]
-    """
+        years = args.years
+        print(f"Using years: {' '.join(sorted(args.years))}")
+    datasets = get_datasets(f, args.hist, samples[0], years)
 
-    # Validate categories
+    # Assume all datasets have the same categories
+    valid_cats = get_categories(f, args.hist, samples[0], datasets[0])
+    if args.category and not args.category in valid_cats:
+        print(f"\nUnknown category '{args.category}' selected."
+              f" Choose from:\n  {'\n  '.join(valid_cats)}")
+        sys.exit(1)
+    elif not args.category:
+        print("No category chosen. Defauling to baseline category")
+        category = "baseline"
+    else:
+        print(f"Using category: {args.category}")
+        category = args.category
 
-    plot(
-        f,
-        args.output,
-        args.variable,
-        args.sample,
-        args.year,
-        args.category,
-        True,
-        # normalize=args.normalize,
-        xlim=(args.xmin, args.xmax),
-        ylim=(args.ymin, args.ymax),
-        xlog=args.xlog,
-        ylog=args.ylog,
-        xstart=args.xstart,
-    )
+    is_data_list = [get_is_data(f, d) for d in datasets]
+    if all(is_data_list):
+        is_data = True
+    elif any(is_data_list):
+        print("mixed MC and data datasets chosen")
+        sys.exit(1)
+    else:
+        is_data = False
+
+    h = get_combined_hist(f, args.hist, samples, datasets, category, is_data)
+
+    kwargs = vars(args).copy()
+    kwargs.pop("output", None)
+
+    # TODO: integrate axes based on xvar/yvar
+    # Warn if 2D arguments were supplied
+
+    # h = apply_common_args_to_hist(h, **kwargs)
+
+    if len(h.axes) == 1:
+        plot(h, args.output, is_data, **kwargs)
+    else:
+        h = h.project("e1_d0", "mu1_d0")
+        plot2d(h, args.output, is_data, **kwargs)
 
 
 if __name__ == "__main__":
--- scripts/plotting/util.py
+++ scripts/plotting/util.py
@@ -1,15 +1,22 @@
-def apply_common_args(ax, **kwargs):
-    if kwargs["xlim"][0] is not None:
-        ax.set_xlim(left=kwargs["xlim"][0])
+import hist
 
-    if kwargs["xlim"][1] is not None:
-        ax.set_xlim(right=kwargs["xlim"][1])
+def apply_common_args_to_hist(h, **kwargs):
+    xstart = hist.loc(0 if not kwargs["xstart"] else kwargs["xstart"])
+    # ystart = hist.loc(0 if not kwargs["ystart"] else kwargs["ystart"])
+    return h[xstart:hist.loc(kwargs["xend"])]
 
-    if kwargs["ylim"][0] is not None:
-        ax.set_ylim(bottom=kwargs["ylim"][0])
+def apply_common_args_to_ax(ax, **kwargs):
+    if kwargs["xmin"] is not None:
+        ax.set_xlim(left=kwargs["xmin"])
 
-    if kwargs["ylim"][1] is not None:
-        ax.set_ylim(top=kwargs["ylim"][1])
+    if kwargs["xmax"] is not None:
+        ax.set_xlim(right=kwargs["xmax"])
+
+    if kwargs["ymin"] is not None:
+        ax.set_ylim(bottom=kwargs["ymin"])
+
+    if kwargs["ymax"] is not None:
+        ax.set_ylim(top=kwargs["ymax"])
 
     if kwargs["xlog"]:
         ax.set_xscale("log")
@@ -18,15 +25,53 @@ def apply_common_args(ax, **kwargs):
         ax.set_yscale("log")
 
 def add_common_args(parser):
-    parser.add_argument("--xmin", type=float, default=None, help="Minimum x-axis value")
-    parser.add_argument("--xmax", type=float, default=None, help="Maximum x-axis value")
-    parser.add_argument("--ymin", type=float, default=None, help="Minimum y-axis value")
-    parser.add_argument("--ymax", type=float, default=None, help="Maximum y-axis value")
+    parser.add_argument("--xmin", type=float, help="Minimum x-axis value")
+    parser.add_argument("--xmax", type=float, help="Maximum x-axis value")
+    parser.add_argument("--ymin", type=float, help="Minimum y-axis value")
+    parser.add_argument("--ymax", type=float, help="Maximum y-axis value")
     parser.add_argument("--ylog", action="store_true", help="Use logarithmic scale for y-axis")
     parser.add_argument("--xlog", action="store_true", help="Use logarithmic scale for x-axis")
-    parser.add_argument("--xstart", type=float, default=None, help="Starting x-axis value for plot range")
-    parser.add_argument("--xend", type=float, default=None, help="Ending x-axis value for plot range")
-    parser.add_argument("--ystart", type=float, default=None, help="Starting y-axis value for plot range")
-    parser.add_argument("--yend", type=float, default=None, help="Ending y-axis value for plot range")
+    parser.add_argument("--xstart", type=float, help="Starting x-axis value for plot range")
+    parser.add_argument("--xend", type=float, help="Ending x-axis value for plot range")
+    parser.add_argument("--ystart", type=float, help="Starting y-axis value for plot range")
+    parser.add_argument("--yend", type=float, help="Ending y-axis value for plot range")
 
 
+def extract_year(year_key):
+    return year_key.split("_")[1]
+
+
+def get_variables(f):
+    return list(f["variables"].keys())
+
+
+def get_samples(f, variable):
+    return list(f["variables"][variable].keys())
+
+
+def get_years(f, variable, sample):
+    return list(set(extract_year(y) for y in f["variables"][variable][sample]))
+
+
+def get_categories(f, variable, sample, year_key):
+    ax = f["variables"][variable][sample][year_key].axes[0]
+    return [ax.value(i) for i in range(ax.extent - 1)]
+
+
+def get_datasets(f, variable, sample, years):
+    return [key for year in years
+                for key in f["variables"][variable][sample].keys()
+            if year in key]
+
+
+def get_combined_hist(f, variable, samples, datasets, category, is_data):
+    if is_data:
+        all_hists = [f["variables"][variable][s][d][category, ...] for s in samples for d in datasets]
+    else:
+        all_hists = [f["variables"][variable][s][d][category, "nominal", ...] for s in samples for d in datasets]
+
+    return sum(all_hists)
+
+
+def get_is_data(f, dataset):
+    return f["datasets_metadata"]["by_dataset"][dataset]["isMC"] == "False"

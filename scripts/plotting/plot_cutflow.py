f2b3f228c09d14fa28d345bc011b8b6a68635b48
Lucas Nestor <nestor.40@buckeyemail.osu.edu>
2026-06-04 15:01:29 -0400

    Finish cutflow plotting script

--- scripts/plotting/plot_cutflow.py
+++ scripts/plotting/plot_cutflow.py
@@ -6,6 +6,8 @@ import sys
 sys.path.append("scripts")
 from coffea_file import CoffeaFile
 
+hep.style.use("CMS")
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -14,27 +16,31 @@ def main():
     parser.add_argument("-y", "--years", nargs="+")
     parser.add_argument("-s", "--sample")
     parser.add_argument("-o", "--output", default="cutflow.png")
+    parser.add_argument("--efficiency", action="store_true")
 
     args = parser.parse_args()
 
     f = CoffeaFile(args.input)
     cutflow = f.get_cutflow(args.category, args.sample, args.years)
+
+    if args.efficiency:
+        cutflow = [c / cutflow[0] for c in cutflow]
+
     labels = ["Total", *f.get_cut_labels(args.category)]
 
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(14,6))
 
-    hep.style.use("CMS")
-    hep.cms.label("Preliminary", ax=ax, data=True, loc=1)
-
+    hep.cms.label("Preliminary", ax=ax, data=True, loc=0)
     hep.histplot(cutflow, ax=ax)
-    # ax.set_xlim(0, len(cutflow))
-    # ax.set_xticks(range(len(cutflow) + 1))
-    # ax.set_xticklabels([])
-    # ax.set_xticks([i + 0.5 for i in range(len(labels))], minor=True)
-    # ax.set_xticklabels(labels, rotation=45, ha="right", minor=True)
-    # ax.tick_params(axis="x", which="minor", length=0)
-    # ax.set_ylabel("Events")
+
+    ax.set_xlim(0, len(cutflow))
+    ax.set_xticks(range(len(cutflow) + 1))
+    ax.set_xticklabels([])
+    ax.set_xticks([i + 0.70 for i in range(len(labels))], minor=True)
+    ax.set_xticklabels(labels, rotation=45, ha="right", minor=True)
+    ax.tick_params(axis="x", which="minor", length=0)
+    ax.set_ylabel("Events")
     ax.set_yscale("log")
 
     fig.savefig(args.output, bbox_inches="tight")

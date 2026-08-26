c4be9cbf4fccc59096c6fe7a686554a10e0bd6b2
Lucas Nestor <nestor.40@osu.edu>
2026-08-26 13:23:21 -0500

    Only use years for the requested sample in get_total_hist
    The old version used all years available in the coffea file. However,
    some samples might have missing years. This change prevents a crash
    by only using the years available for each sample.

--- scripts/coffea_file.py
+++ scripts/coffea_file.py
@@ -15,14 +15,13 @@ class CoffeaFile:
         elif samples is None:
             samples = self.get_samples(hist_name)
 
-        if years is not None:
-            year_keys = [yk for yk in year_keys if self._dataset_year(yk) in years]
-        else:
-            year_keys = self._get_year_keys(hist_name, samples)
-
         category = category if category is not None else "baseline"
 
         for sample in samples:
+            year_keys = self._get_year_keys(hist_name, sample)
+            if years is not None:
+                year_keys = [yk for yk in year_keys if self._dataset_year(yk) in years]
+
             for yk in year_keys:
                 hists.append(self.get_hist(hist_name, sample, yk, category))
 
